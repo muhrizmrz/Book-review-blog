8074db8d596268b862f107b757077ddb8e57c06f
Muhriz <muhrizmadathil@gmail.com>
2023-01-17 06:40:34 +0530

    validator feature started

--- public/javascript/validator.js
+++ public/javascript/validator.js
@@ -1,15 +1,50 @@
 $(document).ready(function(){
-    var book_name = $("input[name|='book_name'").val()
-    var author = $("input[name|='author'").val()
-    var reviewer = $("input[name|='reviewer'").val()
-    var review = $("input[name|='review'").val()
-    var image = $("input[name|='image")[0]
-    var errorMsg = ''
-    $('#add_review').submit((e)=>{
-        if(image.files.length == 0){ 
-            e.preventDefault()
-            errorMsg = "Please Upload image"
-            alert(errorMsg)
+    
+    
+    function validator(event){
+
+        $('.error').remove()
+
+        var book_name = $("input[name|='book_name'")
+        var author = $("input[name|='author'")
+        var reviewer = $("input[name|='reviewer'")
+        var review = $("input[name|='review'")
+        var image = $("#review_image")
+        var errorMsg = ''
+        var errState = []
+
+
+        function validating(elementValue,msg,e){
+            console.log(elementValue.val().length)
+            if(elementValue.val().length === 0){
+                e.preventDefault()
+                elementValue.after(`<small class="error text-red-500">${msg}</small>`)
+                errState.push(true)
+            }
         }
+
+        function validateImage(){
+            console.log(image)
+            if(image.files.length === 0){
+                event.preventDefault()
+                alert('Image field is empty')
+                errState.push(true)
+            }
+        }
+
+        validating(book_name,"Book name is required",event)
+        validating(author, "Author name is required",event)
+        validating(review, "Review is required",event)
+        validating(reviewer, "Reviewer is required",event)
+
+        validateImage()
+
+        !errState.includes(true) ? alert("Added") : ''
+
+    }
+
+    $('#add_review').submit(function(e){    
+        validator(e)    
     })
+
 })
